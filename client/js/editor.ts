1c14df4b5a58354541bcdc7a485ba2c9f2fcf8c9
coder6583 <thedude6583@gmail.com>
2021-08-16 19:33:36 +0900

    delete memory buffer update

--- client/js/editor.ts
+++ client/js/editor.ts
@@ -622,7 +622,6 @@ socket.on('compileFinished', (result: { success: boolean; wasm: string }) => {
 		logConsole('---------- START ----------');
 		fetch(result.wasm)
 			.then((response) => {
-				memory = new WebAssembly.Memory({initial: 17, shared: true});
 				return response.arrayBuffer();
 			})
 			.then((bytes) => WebAssembly.instantiate(bytes, importObject))
